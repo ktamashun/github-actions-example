829055875e80513322d3aeec84908e049a6f4260
Tamás Kovács <kovacs.tamas.hun@gmail.com>
2020-07-14 22:43:38 +0200

    test(main) Add new test for February

--- main_test.go
+++ main_test.go
@@ -12,6 +12,16 @@ func TestGetMonthNameJanuary(t *testing.T) {
 		t.Errorf("getMonthName(1) exected January, got %s\n", monthName)
 	}
 }
+func TestGetMonthNameFebruary(t *testing.T) {
+	monthName, err := getMonthName(2)
+	if err != nil {
+		t.Errorf("getMonthName(2) got an error: %s\n", err)
+	}
+
+	if monthName != "February" {
+		t.Errorf("getMonthName(2) exected February, got %s\n", monthName)
+	}
+}
 func TestGetMonthNameInvalidMonth(t *testing.T) {
 	_, err := getMonthName(13)
 	if err == nil {
